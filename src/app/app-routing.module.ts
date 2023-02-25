f3a62f4165d925b891a80c1b265600f8f657782e
Sanghamitra Biswas <sangy.987@gmail.com>
2023-02-25 10:02:47 +0530

    layout changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import { Tab3ComponentComponent } from './components/tab3-component/tab3-compone
 import { Tab4ComponentComponent } from './components/tab4-component/tab4-component.component';
 
 const routes: Routes = [
-  { path: '/tab1', component: Tab1ComponentComponent },
-  { path: '/tab2', component: Tab2ComponentComponent },
-  { path: '/tab3', component: Tab3ComponentComponent },
-  { path: '/tab4', component: Tab4ComponentComponent },
+  { path: '/#tab1', component: Tab1ComponentComponent },
+  { path: '/#tab2', component: Tab2ComponentComponent },
+  { path: '/#tab3', component: Tab3ComponentComponent },
+  { path: '/#tab4', component: Tab4ComponentComponent },
 
 ];
 
--- src/app/components/vertical-tabs/vertical-tabs.component.ts
+++ src/app/components/vertical-tabs/vertical-tabs.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
-  selector: 'app-vertical-tabs',
-  templateUrl: './vertical-tabs.component.html',
-  styleUrls: ['./vertical-tabs.component.scss'],
-  providers: [NgbModalConfig, NgbModal],
+	selector: 'app-vertical-tabs',
+	templateUrl: './vertical-tabs.component.html',
+	styleUrls: ['./vertical-tabs.component.scss'],
+	providers: [NgbModalConfig, NgbModal],
 })
 export class VerticalTabsComponent {
-  constructor(config: NgbModalConfig, private modalService: NgbModal) {
-		// customize default values of modals used by this component tree
+	constructor(config: NgbModalConfig, private modalService: NgbModal) {
 		config.backdrop = 'static';
 		config.keyboard = false;
 	}
-  value1!: string;
+	value1!: string;
+	remainingText!: number;
+	value!: string;
+	value2!: string;
+	value3!: string;
 
-	open(content:any) {
+
+	open(content: any) {
 		this.modalService.open(content);
+	}
+	valueChange(value: string) {
+		this.remainingText = 1024 - value.length;
 	}
 }
